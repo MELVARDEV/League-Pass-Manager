8df068f7ec2185e31f7f071faa4d2481a0a2cb21
MELVAR <melvar@exory.dev>
2023-04-25 15:18:38 +0200

    Update TitleBar.tsx

--- manager/src/renderer/components/title_bar/TitleBar.tsx
+++ manager/src/renderer/components/title_bar/TitleBar.tsx
@@ -1,6 +1,11 @@
 export default function TitleBar() {
   return (
-    <div id="titleBar" style={{}}>
+    <div
+      id="titleBar"
+      style={{
+        borderBottom: '1px solid rgba(245, 245, 255, 0.12)',
+      }}
+    >
       title
     </div>
   );
